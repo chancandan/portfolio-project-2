d80105c5ebd78b076cf1e589efc7b342485ef942
chancandan <daniellyons@live.ie>
2023-02-19 15:09:52 +0000

    Create game and user variables and functions for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,31 @@ const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const scissors_div = document.getElementById("sciss");
 
+function getComputerChoice() {
+const choices = ['r', 'p', 'sciss'];
+const randomNumber(Math.floor(Math.random() * 3));
+return choices[randomNumber];
+}
+getComputerChoice()
+
+function game(userChoice) {
+    
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+    game("r");
+})
+
+    paper_div.addEventListener('click', function() {
+    game("p");
+})
+
+    scissors_div.addEventListener('click', function() {
+    game("sciss");
+})
+}
+
+main();
